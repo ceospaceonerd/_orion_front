56c2ca0639ab3cdd134495d8bcd1c9d3e87282d8
Raymol Mateo <djran360@gmail.com>
2023-06-03 21:35:10 +0000

    Update 5 files

--- src/@fake-db/apps/comapnyInfo.js
+++ src/@fake-db/apps/comapnyInfo.js
@@ -19,8 +19,8 @@ const data ={
       currency:'DOP',
       businesshours: [{
         id:1,
-        day:'primary',
-        open:'9:00 AM'
+        day:'Monday',
+        open:'9:00 AM',
         close :'6:00 PM'}]
     }]
 }
@@ -37,7 +37,7 @@ mock.onPost('/register/company/add-company').reply(config => {
   }
   user.id = lastIndex + 1
   data.users.unshift({ ...company, logo:'',rnc: '', ceo: '', name: '',address:'',fiscalyear:'',country:'',contact:'',email:'',
-  currency:'', businesshours:[id:'',open:'',close:'']})
+  currency:''})
 
   return [201, { user }]
 })
